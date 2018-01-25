7f1b9ed7ac7a1af9f7adcae20f7f1c7fd68da692
Egor Kuzmichev <yegor.kuzmichev@gmail.com>
2018-01-25 12:26:31 +0300

    getfiles

---
+++ get_titles.php
@@ -0,0 +1,41 @@
+<?php
+/* Заполняем заголовки в базе данных
+   (данные берем из страниц)
+*/
+
+ini_set("display_errors",0);
+$url = "http://php.net/manual/ru/domdocument.loadhtmlfile.php";
+
+$doc = new DOMDocument();
+$doc->loadHTML(file_get_contents($url));
+$elements=$doc->getElementsByTagName('title');
+
+echo $elements[0]->nodeValue;
+
+
+$db = new SQLite3("database.sqlite");
+
+$result=$db->query('SELECT rowid, url FROM urls');
+while ($res = $result->fetchArray()) {
+	$rowid=$res["rowid"];
+	echo "\n\n";
+	echo $rowid;
+	echo "\n";
+	echo $res["url"];
+	echo "\n";
+	$title = get_title($res["url"]);
+	echo "$title";
+	$db->query("UPDATE urls SET title=$title WHERE rowid=$rowid");
+}
+
+
+function get_title($url) {
+	$doc = new DOMDocument();
+	$doc->loadHTML(file_get_contents($url));
+	$elements=$doc->getElementsByTagName('title');
+	echo $elements[0]->nodeValue;
+}
+
+
+
+?>
