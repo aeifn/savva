c6a553f17e0a5ccf0722bb19c12e3e7ea646eb71
Egor Kuzmichev <yegor.kuzmichev@gmail.com>
2018-01-30 19:35:07 +0300

    docroot

--- add/add.php
+++ add/add.php
@@ -5,7 +5,7 @@ require_once $_SERVER["DOCUMENT_ROOT"] . "/include/functions.inc";
 require_once $_SERVER["DOCUMENT_ROOT"] . "/models/model.inc";
 
 
-$model = new Model("../database.sqlite");
+$model = new Model($_SERVER["DOCUMENT_ROOT"]."/database.sqlite");
 
 $url = $_GET["url"];
 $title = get_title($url);
